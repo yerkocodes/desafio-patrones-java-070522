f7279632eff8ddf7d206d16d7b14e7c48b4896fe
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-05-11 10:45:33 -0400

    Square pattern method finished

--- src/PatronesAnidados.java
+++ src/PatronesAnidados.java
@@ -5,24 +5,52 @@ public class PatronesAnidados {
         Scanner scInt = new Scanner(System.in);
         int userNumberOption;
 
+        System.out.print("Seleccione cual patron desea ejecutar: \n Presione 1 para el primer patron. \n Presione 2 para el segundo patron. \n Presione 3 para el tercer patron. \n Presione 4 para el cuarto patron. \n");
+        userNumberOption = scInt.nextInt();
+
+        switch (userNumberOption) {
+            case 1:
+                System.out.print("Ingrese un numero a iterar: \n");
+                userNumberOption = scInt.nextInt();
+                squarePattern(userNumberOption);
+                break;
+/*
+            case 2:
+                //Instructions
+                break;
+            case 3:
+                //Instructions
+                break;
+            case 4:
+                //Instructions
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + userNumberOption);
+*/
+        }
+
+/*
         System.out.print("Ingrese un numero: \n");
         userNumberOption = scInt.nextInt();
 
         patronUno(userNumberOption);
+*/
     }
 
-    private static void patronUno( int userNumber ) {
+    private static void squarePattern(int userNumber ) {
 
         for ( int i = 1; i <= userNumber; i++ ) {
             for ( int j = 1; j <= userNumber; j++ ) {
                 if ( i == 1 || j == 1 || i == userNumber || j == userNumber ) {
-                    System.out.printf("*");
+                    System.out.print("*");
                 } else {
-                    System.out.printf(" ");
+                    System.out.print(" ");
                 }
             }
-            System.out.printf("\n");
+            System.out.print("\n");
         }
+        System.out.print("Patron finalizado con exito!");
 
     }
+
 }
